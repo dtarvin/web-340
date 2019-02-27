8120f639d883293aa88cc25ee01d4f49fb03e9ea
David Tarvin <david.tarvin@physiciansmutual.com>
2019-02-27 14:57:04 -0600

    fix spacing on exercise 2.2

--- week-2/hello-world/tarvin-exercise-2.2.js
+++ week-2/hello-world/tarvin-exercise-2.2.js
@@ -4,8 +4,8 @@ var http = require("http");
 var app = express();
 
 app.use(function(request, response) {
-    console.log("In comes a request to: " + request.url);
-    response.end("Hello World");
+  console.log("In comes a request to: " + request.url);
+  response.end("Hello World");
 });
 
 http.createServer(app).listen(8080);
