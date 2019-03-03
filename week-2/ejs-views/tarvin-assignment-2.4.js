261617c8edef2d23a0d01dbfb1833701f24bbf8d
David R Tarvin Jr <david.tarvin@gmail.com>
2019-03-03 14:05:35 -0600

    edits to assignment 2.4

--- week-2/ejs-views/tarvin-assignment-2.4.js
+++ week-2/ejs-views/tarvin-assignment-2.4.js
@@ -7,7 +7,16 @@ var app = express();
 app.set("views", path.resolve(__dirname, "views")); // Tell Express the views are in the 'views' directory
 app.set("view engine", "ejs"); // Tell Express to use the EJS view engine
 app.get("/", function(request, response) {
+  // response.render("index", {
+  //   firstName: "David",
+  //   lastName: "Tarvin",
+  //   address: "123 Rosy Lane",
+  //   city: "Omaha",
+  //   state: "NE",
+  //   zip: "68123"
+  // });
   response.render("index", {
+    firstName: "David",
     message: "Welcome to the homepage!"
   });
 });
