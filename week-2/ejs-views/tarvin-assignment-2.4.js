1276bc0e00a88fec32f2ec37e035158b29763356
David R Tarvin Jr <david.tarvin@gmail.com>
2019-03-03 13:01:50 -0600

    adding part of assignment 2.4

---
+++ week-2/ejs-views/tarvin-assignment-2.4.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views")); // Tell Express the views are in the 'views' directory
+app.set("view engine", "ejs"); // Tell Express to use the EJS view engine
+app.get("/", function(request, response) {
+  response.render("index", {
+    message: "Welcome to the homepage!"
+  });
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("EJS-Views app started on port 8080.");
+});
