b92eb990906d8751c95341baa6656704f1f63473
David R Tarvin Jr <david.tarvin@gmail.com>
2019-03-17 16:50:05 -0500

    add exercise 4.3

---
+++ week-4/status-codes/tarvin-exercise-4.3.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var http = require("http");
+
+var app = express();
+
+app.get("/not-found", function(request, response) {
+  response.status(404);
+  response.json({
+    error: "Resource not found."
+  })
+});
+
+app.get("/ok", function(request, response) {
+  response.status(200);
+  response.json({
+    message: "Page loaded correctly."
+  })
+});
+
+app.get("/not-implemented", function(request, response) {
+  response.status(501);
+  response.json({
+    error: "Page not implemented."
+  })
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
