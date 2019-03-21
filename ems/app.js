4c771be941e41a68947c4c1d5a2efb13d02782d3
David Tarvin <david.tarvin@physiciansmutual.com>
2019-03-21 14:39:22 -0500

    Add app.js

---
+++ ems/app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var logger = require("morgan");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.use(logger("short"));
+
+app.get("/", function(request, response) {
+  response.render("index", {
+    title: "Home page"
+  });
+});
+
+http.createServer(app).listen(8080, function() {
+  console.log("Application started on port 8080!");
+});
