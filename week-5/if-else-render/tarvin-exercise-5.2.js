63a6a5986ad30591ead095b16b21a608bbaa1e6a
David Tarvin <david.tarvin@physiciansmutual.com>
2019-03-21 14:26:16 -0500

    fix indenting

--- week-5/if-else-render/tarvin-exercise-5.2.js
+++ week-5/if-else-render/tarvin-exercise-5.2.js
@@ -9,18 +9,18 @@ app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 
 var n = [
-    "Harry",
-    "Sally",
-    "Jenny",
-    "Forrest"
+  "Harry",
+  "Sally",
+  "Jenny",
+  "Forrest"
 ];
 
 app.get("/", function(request, response) {
-    response.render("index", {
-        names:n
-    })
+  response.render("index", {
+    names:n
+  })
 });
 
 http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080!");
+  console.log("Application started on port 8080!");
 })
