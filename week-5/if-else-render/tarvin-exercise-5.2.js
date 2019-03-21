b333eeb2662e51880db215db03c6503e6b068a0d
David Tarvin <david.tarvin@physiciansmutual.com>
2019-03-21 14:13:08 -0500

    add exercise 5.2

---
+++ week-5/if-else-render/tarvin-exercise-5.2.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var http = require("http");
+var path = require("path");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+var n = [
+    "Harry",
+    "Sally",
+    "Jenny",
+    "Forrest"
+];
+
+app.get("/", function(request, response) {
+    response.render("index", {
+        names:n
+    })
+});
+
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port 8080!");
+})
