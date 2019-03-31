eee97c4067a47b35050806bc3c6927061580a6a1
David R Tarvin Jr <david.tarvin@gmail.com>
2019-03-31 13:58:29 -0500

    commit

--- exercises-from-book/Chapter_8/learn-about-me/routes.js
+++ exercises-from-book/Chapter_8/learn-about-me/routes.js
@@ -38,7 +38,7 @@ router.post("/signup", function(req, res, next) {
     }
 
     var newUser = new User({
-      username: username;
+      username: username,
       password: password
     });
     newUser.save(next);
@@ -67,5 +67,36 @@ router.post("/login", passport.authenticate("login", {
   failureRedirect: "/login",
   failureFlash: true
 }));
+
+router.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash("info", "You must be logged in to see this page.");
+    res.redirect("/login");
+  }
+}
+
+router.get("/edit", ensureAuthenticated, function(req, res) {
+  res.render("edit");
+});
+
+router.post("/edit", ensureAuthenticated, function(req, res, next) {
+  req.user.displayName = req.body.displayName;
+  req.user.bio = req.body.bio;
+  req.user.save(function(err) {
+    if (err) {
+      next(err);
+      return;
+    }
+    req.flash("info", "Profile updated!");
+    res.redirect("/edit");
+  });
+});
 
 module.exports = router;
