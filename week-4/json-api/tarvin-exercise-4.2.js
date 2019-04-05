8318963f2cc34b1cc82c29811938d9c0bcc8ae61
David Tarvin <david.tarvin@physiciansmutual.com>
2019-04-05 10:01:16 -0500

    fix code attribution for exercise 4.2

--- week-4/json-api/tarvin-exercise-4.2.js
+++ week-4/json-api/tarvin-exercise-4.2.js
@@ -1,3 +1,17 @@
+/*
+============================================
+; Title:  tarvin-exercise-4.2.js
+; Author: David Tarvin
+; Date:   17 March 2019
+; Description: json-api
+;===========================================
+*/
+
+// display header at beginning of program
+const header = require('../../Tarvin-header.js');
+console.log(header.display("David", "Tarvin", "Exercise 4.2"));
+console.log("");
+
 var express = require("express");
 var http = require("http");
 
