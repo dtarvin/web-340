586d9094630570cbf6b975d3521cbb41c30fdebe
David Tarvin <david.tarvin@physiciansmutual.com>
2019-04-05 12:07:34 -0500

    fix code attribution for exercise 7.3

--- week-7/chai-example/tarvin-fruits.js
+++ week-7/chai-example/tarvin-fruits.js
@@ -1,3 +1,19 @@
+/*
+============================================
+; Title:  tarvin-fruits.js
+; Author: Richard Krasso
+; Modified by: David Tarvin
+; Date:   05 April 2019
+; Description: Mocha and Chai
+;===========================================
+*/
+
+// display header at beginning of program
+const header = require('../../../Tarvin-header.js');
+console.log(header.display("David", "Tarvin", "Exercise 7.3"));
+
+
+
 function fruits(str) {
   return str.split(',');
 }
