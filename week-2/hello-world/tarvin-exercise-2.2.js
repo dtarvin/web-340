8169e70aee92060a477751e6211a37d9a1e41f94
David Tarvin <david.tarvin@physiciansmutual.com>
2019-04-05 09:01:29 -0500

    fix code attribution for exercise 2.2

--- week-2/hello-world/tarvin-exercise-2.2.js
+++ week-2/hello-world/tarvin-exercise-2.2.js
@@ -1,3 +1,17 @@
+/*
+============================================
+; Title:  tarvin-exercise-2.2.js
+; Author: David Tarvin
+; Date:   28 February 2019
+; Description: Hello World
+;===========================================
+*/
+
+// display header at beginning of program
+const header = require('../../Tarvin-header.js');
+console.log(header.display("David", "Tarvin", "Assignment 6.4"));
+console.log("");
+
 var express = require("express");
 var http = require("http");
 
