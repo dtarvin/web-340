528ace9e251d9d510463065abaa0c09518287f37
David Tarvin <david.tarvin@physiciansmutual.com>
2019-04-05 09:57:29 -0500

    fix code attribution for assignment 4.4

--- week-4/crud/tarvin-assignment-4.4.js
+++ week-4/crud/tarvin-assignment-4.4.js
@@ -1,12 +1,18 @@
 /*
 ============================================
-; Title:  app.js
+; Title:  tarvin-assignment-4.4.js
 ; Author: Professor Krasso
-; Date:   28 February 2019
+; Modified by: David Tarvin
+; Date:    17 March 2019
 ; Description: Demonstrates CRUD operations in a
 ;              Node.js API.
 ;===========================================
 */
+
+// display header at beginning of program
+const header = require('../../Tarvin-header.js');
+console.log(header.display("David", "Tarvin", "Assignment 4.4"));
+console.log("");
 
 var express = require("express");
 var http = require("http");
