e22d8a3fa7272cae2c91cd64a02df3f5d08d3ecf
David Tarvin <david.tarvin@physiciansmutual.com>
2019-04-05 11:36:43 -0500

    fix code attribution for exercise 5.3

--- week-5/pug-template/tarvin-exercise-5.3.js
+++ week-5/pug-template/tarvin-exercise-5.3.js
@@ -1,3 +1,17 @@
+/*
+============================================
+; Title:  tarvin-exercise-5.3.js
+; Author: David Tarvin
+; Date:   21 March 2019
+; Description: Pug Template
+;===========================================
+*/
+
+// display header at beginning of program
+const header = require('../../Tarvin-header.js');
+console.log(header.display("David", "Tarvin", "Exercise 5.3"));
+console.log("");
+
 var express = require("express");
 var http = require("http");
 var pug = require("pug");
