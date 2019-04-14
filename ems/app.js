db0dc36d4ef5ce4eb51943997c609646b4eb1207
David R Tarvin Jr <david.tarvin@gmail.com>
2019-04-14 15:55:13 -0500

    commit

--- ems/app.js
+++ ems/app.js
@@ -10,40 +10,60 @@
 
 // display header at beginning of program
 const header = require('../Tarvin-header.js');
-console.log(header.display("David", "Tarvin", "Assignment 6.4 - EMS"));
+console.log(header.display("David", "Tarvin", "EMS"));
 console.log("");
 
-var express = require("express");
-var http = require("http");
-var path = require("path");
-var logger = require("morgan");
-var helmet = require("helmet");
-var mongoose = require("mongoose");
-var Employee = require('./models/employee');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+const logger = require("morgan");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const Employee = require('./models/employee');
 
-var mongoDB = "mongodb+srv://bellevue_student:david1234@cluster0-xzug0.mongodb.net/test?retryWrites=true";
+const mongoDB = "mongodb+srv://bellevue_student:david1234@cluster0-xzug0.mongodb.net/test?retryWrites=true";
 mongoose.connect(mongoDB, {
   useNewUrlParser: true
 });
 
 mongoose.Promise = global.Promise;
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
 db.once("open", function() {
   console.log("Application connected to MongoDB Atlas instance");
 });
 
-var app = express();
+let csrfProtection = csrf({cookie:true});
 
-app.set("views", path.resolve(__dirname, "views"));
+let app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(logger("short"));
 
 app.use(helmet.xssFilter());
+
+app.use(cookieParser());
+
+app.use(csrfProtection);
+
+app.use(function(request,response, next) {
+  let token = request.csrfToken();
+  response.cookie("XSRF-TOKEN", token);
+  response.locals.csrfToken = token;
+  next();
+});
+
+app.set("views", path.resolve(__dirname, "views"));
+
+app.set("view engine", "ejs");
 
 var employee = new Employee({
   firstName: "David",
@@ -53,8 +73,36 @@ var employee = new Employee({
 app.get("/", function(request, response) {
   response.render("index", {
     title: "Home page",
-    message: "XSS Prevention Example"
+    message: "New Employee Entry Page"
   });
+});
+
+app.get("/list", function(request, response) {
+  Employee.find({}, function(error, employees) {
+    if (error) throw error;
+
+    response.render("list", {
+      title: "Employee List",
+      employees: employees
+    });
+  });
+});
+
+app.post("/process", function(request, response) {
+  // console.log(request.body.txtName);
+  if(!request.body.firstName || !request.body.lastName) {
+    response.status(400).send("Entries must have a first and last name");
+    return;
+  }
+
+  const firstName = request.body.firstName;
+  const lastName = request.body.lastName;
+  const newEmployee = new Employee({ firstName, lastName });
+  newEmployee.save(function(error) {
+    if (error) throw error;
+    console.log(firstName + ' ' + lastName + ' saved successfully!');
+  });
+  response.redirect("/");
 });
 
 http.createServer(app).listen(8080, function() {
